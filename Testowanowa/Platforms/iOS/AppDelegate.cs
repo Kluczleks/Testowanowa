1139b5dccefaf0019968f0265bbefacd7c5744a1
1663106 <1663106@TE-GDA-D-D21E07>
2024-09-09 12:29:04 +0200

    Dodaj pliki projektów.

---
+++ Testowanowa/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Testowanowa
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
